b826456e4fe1a8452b1c36edaaecb6ad716a0323
MD Eftekhar Khan <mdeftekhar.khan1@gmail.com>
2025-10-22 17:50:02 +0600

    Day_01 is Done

---
+++ Day_01.py
@@ -0,0 +1,44 @@
+#Day:01 Topic
+# 1. What is Python, why and how to setup the environment?
+# 2. First python program run and Explanation step by step.
+# 3. Python Comments: single &  multi-line or (Block) comments
+# 4. Python Variable: rules & usages
+
+#3. In python single & multi-line or block comment syntax are same.
+#This is a single line comment 
+#This is a multi-line or block comment
+
+# 4. Python Variable: rules & usages
+
+#4.1. No explicit declaration needed
+    # i. A variable is created when you assign a value with =
+    # ii. Example:
+# x = 10
+# name = "Alice"
+
+#4.2. Naming Rules
+
+   # i. Variable can contain letters(A-Z), (a-z), digit(0-9), Underscore(_)
+   # ii. Can't start with digit
+   # ✅ age1 = 25
+   # ❌ 1age = 25
+
+#4.3. Dynamic Typing
+   #i. Python infers the type from the value assigned.
+# x = 5         int
+# x = "hello"   now str
+
+#4.4. Reassignment allowed:
+    #.i. You can change the value or even the type of a variable at any time.
+
+#4.5. Special characters not allowed:
+    #i. Only _ is permitted. No spaces, hyphens, or symbols.
+
+# ❌ user-name = "Tom"
+# ✅ user_name = "Tom"
+
+#4.6. Convention (not enforced, but recommended):
+    # i. Use snake_case for variable names: total_price, user_age.
+    # ii. Constants are written in UPPERCASE: PI = 3.14159.
+
+ 
